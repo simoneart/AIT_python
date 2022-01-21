28e6bf05fbb91936b6f5ddd25da13a8aacb62be2
simoneart <95907355+simoneart@users.noreply.github.com>
2022-01-21 10:48:37 +0000

    last few things

--- src/librerie/ex1.py
+++ src/librerie/ex1.py
@@ -9,3 +9,9 @@ print("Autoincrement b +=1 b=", b)
 print("Multiply c*3 " ,c, "* 3= ",c*3)
 print("Sin (c)", np.sin(c))
 
+#slicing
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a[0][0])
+print(a[:,1])
+print(a[0,:])
+print(a[2:,1:3])
